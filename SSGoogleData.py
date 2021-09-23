25ef7f9b7c59e9f488633144c7b75e5274a09946
Tae Lee <tae.leeh@gmail.com>
2021-09-22 21:35:10 -0400

    Rank distribution into dictionary

--- SSGoogleData.py
+++ SSGoogleData.py
@@ -1,9 +1,11 @@
 #  pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib to get google api pip install
-import gspread,time
+import gspread, time
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
+from collections import defaultdict
 
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("token.json", scope)
 
@@ -11,40 +13,33 @@ client = gspread.authorize(creds)
 
 sheet = client.open("Scrub Series IV (Responses)").sheet1  # Open the spreadhseet
 
-#data = sheet.get_all_records()  # Get a list of all records
+data = sheet.get_all_values()  # Get a list of all records
 
-#row = sheet.row_values(3)  # Get a specific row
-#col = sheet.col_values(3)  # Get a specific column
-#cell = sheet.cell(1,2).value  # Get the value of a specific cell
-Players = {
-    'iron':None,
-    'bronze':None,
-    'silver':None,
-    'gold':None,
-    'platinum':None,
-    'diamond':None,
-    'immortal':None,
-    'radiant':None
+Player_Ranks = {
+    'Iron': {'1': [], '2': [], '3': []},
+    'Bronze': {'1': [], '2': [], '3': []},
+    'Silver': {'1': [], '2': [], '3': []},
+    'Gold': {'1': [], '2': [], '3': []},
+    'Platinum': {'1': [], '2': [], '3': []},
+    'Diamond': {'1': [], '2': [], '3': []},
+    'Immortal': [],
+    'Radiant': []
 }
 
-numRows = sheet.row_count  # Get the number of rows in the sheet
-numCol = sheet.col_count
 
+for i in range(1, len(data)):
 
-for i in range(1,numCol):
-    if sheet.cell(1, i).value != None: #sheet.cell(row,column)
-        if sheet.cell(1,i).value == 'Current Rank': #column of the riot ID based off of rank
-            colOfRanks = i #Column of the ranks associated by players
+    try:
+        Rank_Name, Rank_Number = data[i][3].split()
 
-for j in range(1,numRows):
-    if sheet.cell(j, colOfRanks).value is not None and sheet.cell(j, colOfRanks).value != 'Current Rank':
-        colOfPlayers = colOfRanks-1
-        pprint(sheet.cell(j, colOfPlayers).value)
+    except ValueError:# when its missing Rank_number aka immortal and radiant
+        Rank_Name = data[i][3]
+        Rank_Number = None
 
-        for ranks in Players.keys():
-           if sheet.cell(j, colOfRanks).value in ranks:
-               Players[ranks].append()
+    if Rank_Number is not None:
+        Player_Ranks[Rank_Name][Rank_Number].append(data[i][2])
 
+    elif Rank_Name != '':
+        Player_Ranks[Rank_Name].append(data[i][2])
 
-
-
+pprint(Player_Ranks)
